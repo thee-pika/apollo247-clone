2a88579ab33088dc2aa1fac2117da9329209c81d
Mogili Deepika <mogilideepika218@gmail.com>
2025-05-05 11:59:40 +0530

    added dist to gitignore

--- backend/dist/controller/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from "express";
-export declare const AddDoctor: (req: Request, res: Response) => Promise<void>;
-export declare const listAllDoctors: (req: Request, res: Response) => Promise<void>;
--- backend/dist/routes/route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const controller_1 = require("../controller");
-exports.router = (0, express_1.Router)();
-exports.router.post("/", controller_1.AddDoctor);
-exports.router.get("/", controller_1.listAllDoctors);
-//# sourceMappingURL=route.js.map
